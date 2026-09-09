c5bc958f62b97e9435c846515bf48032d485302f
Máté Zoltán Farkas <mate.farkas@rwth-aachen.de>
2026-09-09 23:20:54 +0200

    removed redundancy correction

--- renderers/base.py
+++ renderers/base.py
@@ -26,15 +26,15 @@ class BaseRenderer:
     #     return markdown
     #
     def handle_h2(self, element: bs4.element.Tag, *args, **kwargs) -> str:
-        markdown = "\n## " + element.get_text() + "\n"
+        markdown = "\n# " + element.get_text() + "\n"
         return markdown
 
     def handle_h3(self, element: bs4.element.Tag, *args, **kwargs) -> str:
-        markdown = "\n### " + element.get_text() + "\n"
+        markdown = "\n## " + element.get_text() + "\n"
         return markdown
 
     def handle_h4(self, element: bs4.element.Tag, *args, **kwargs) -> str:
-        markdown = "\n#### " + element.get_text() + "\n"
+        markdown = "\n### " + element.get_text() + "\n"
         return markdown
 
     def handle_p(self, element: bs4.element.Tag, *args, **kwargs) -> str:
